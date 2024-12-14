d0563c2a0b7e9f70ff48953387c1eb5faca912ad
luismoralesunach <luismorales2914@gmail.com>
2024-12-14 02:37:30 -0600

    fix for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Expose the server to all interfaces
-    port: 5173, // Ensure you're using the correct port
-  },
 })
